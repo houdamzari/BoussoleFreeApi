4ebaeaeaf3d16c5ff1a9b8655431b8de2597a7f8
houdamzari <73146419+houdamzari@users.noreply.github.com>
2021-05-28 00:04:40 +0100

    validation-added-regex

--- app/Http/Controllers/UserrsController.php
+++ app/Http/Controllers/UserrsController.php
@@ -14,14 +14,20 @@ class UserrsController extends Controller
     }
     public function store(Request $request)
     {
+        $request->validate([
+            'firstname' => 'required|regex:/^[A-Z][^A-Z]*$/',
+            'lastname' => 'required|regex:/[A-Z]+\[a-z]/',
+    ]);
         $userr = new Userrs;
         $userr->firstname = $request->firstname;
         $userr->lastname = $request->lastname;
         $userr->save();
         return redirect('/redirect')->with('status', 'Blog Post Form Data Has Been inserted');
+
     }
     public function redirect(){
         $users = DB::select('select * from userrs ORDER BY id DESC limit 1');
         return view('redirect',['user'=>$users]);
 
-        }}
+        }
+    }
